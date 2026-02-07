4c1b4e5b673270e4b8f9f1e681cd7de2b5a5d7f3
Jonathan Imanuel <jonathanimanuel023@gmail.com>
2026-02-07 19:06:11 +0700

    fixing DataTable Trait

--- app/Traits/DataTable.php
+++ app/Traits/DataTable.php
@@ -33,7 +33,7 @@ trait DataTable {
 
   public static function bootDataTable() {
     self::saved(function ($model) {
-      if (! $model->deleted_at) {
+      if (!$model->deleted_at) {
         return;
       }
       ModelConnection::where(function ($query) use ($model) {
@@ -96,7 +96,7 @@ trait DataTable {
     if (get_class($this) == Log::class) {
       return;
     }
-    if (! $this->dataBefore) {
+    if (!$this->dataBefore) {
       return;
     }
     $this->loadRelations();
@@ -253,7 +253,7 @@ trait DataTable {
     foreach ($relations as $key => $relation) {
       $relationName = is_int($key) ? $relation : $key;
 
-      if (! method_exists($this, $relationName)) {
+      if (!method_exists($this, $relationName)) {
         $toLoad[$key] = $relation;
         continue;
       }
@@ -332,53 +332,53 @@ trait DataTable {
     $alias     = static::$alias ??
       \ucwords(str_replace(['_', '-'], ' ', Str::snake($nameModel)));
     $module    = static::$module ?? Str::afterLast(Str::before(static::class, '\\' . $nameModel), "\\");
-    if (! $module) {
+    if (!$module) {
       \print_r("\e[39m" . static::class . " \e[91m(Module name not found) \e[39m" . \PHP_EOL);
       return;
     }
     if (static::$is_submitable ?? false) {
-      if (! Schema::hasColumn($tableName, 'code')) {
+      if (!Schema::hasColumn($tableName, 'code')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->string('code')->unique();
         });
       }
-      if (! Schema::hasColumn($tableName, 'branch_id')) {
+      if (!Schema::hasColumn($tableName, 'branch_id')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->foreignUlid('branch_id')->nullable()->references('id')->on('branches')->nullOnDelete();
         });
       }
 
-      if (! Schema::hasColumn($tableName, 'status')) {
+      if (!Schema::hasColumn($tableName, 'status')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->json('status')->nullable();
         });
       }
-      if (! Schema::hasColumn($tableName, 'created_by')) {
+      if (!Schema::hasColumn($tableName, 'created_by')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->foreignUlid('created_by')->references('id')->on('users')->restrictOnDelete();
         });
       }
-      if (! Schema::hasColumn($tableName, 'submitted_at')) {
+      if (!Schema::hasColumn($tableName, 'submitted_at')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->timestamp('submitted_at')->nullable();
         });
       }
-      if (! Schema::hasColumn($tableName, 'canceled_at')) {
+      if (!Schema::hasColumn($tableName, 'canceled_at')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->timestamp('canceled_at')->nullable();
         });
       }
-      if (! Schema::hasColumn($tableName, 'revision_number')) {
+      if (!Schema::hasColumn($tableName, 'revision_number')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->unsignedTinyInteger('revision_number')->default(0);
         });
       }
-      if (! Schema::hasColumn($tableName, 'amended_from_id')) {
+      if (!Schema::hasColumn($tableName, 'amended_from_id')) {
         Schema::table($tableName, function (Blueprint $table) use ($tableName) {
           $table->foreignUlid('amended_from_id')->nullable()->references('id')->on($tableName)->nullOnDelete();
         });
       }
-      if (! Schema::hasColumn($tableName, 'additional_data')) {
+      if (!Schema::hasColumn($tableName, 'additional_data')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->json('additional_data')->nullable();
         });
@@ -391,7 +391,7 @@ trait DataTable {
       }
 
       $formatingSeries = FormatingSeries::where('model', static::class)->first();
-      if (! $formatingSeries) {
+      if (!$formatingSeries) {
         FormatingSeries::create([
           'model'  => static::class,
           'name'   => Str::singular($alias),
@@ -406,7 +406,7 @@ trait DataTable {
       } else {
         $logs = (array) $formatingSeries->logs;
         $key  = (new FormatingSeriesService())->getKeyLogsForInit(static::class, static::$defaultFormatCode ?? '@[iiii]');
-        if (! \array_key_exists($key, $logs)) {
+        if (!\array_key_exists($key, $logs)) {
           $logs[$key] = [
             'current'    => 0,
             'updated_at' => now(),
@@ -452,19 +452,19 @@ trait DataTable {
         });
       }
 
-      if (! Schema::hasColumn($tableName, 'have_transactions')) {
+      if (!Schema::hasColumn($tableName, 'have_transactions')) {
         Schema::table($tableName, function (Blueprint $table) {
           $table->boolean('have_transactions')->default(false);
         });
       }
     }
     if (static::$is_tree_view ?? false) {
-      if (! Schema::hasColumn($tableName, 'parent_id')) {
+      if (!Schema::hasColumn($tableName, 'parent_id')) {
         Schema::table($tableName, function (Blueprint $table) use ($tableName) {
           $table->foreignUlid('parent_id')->nullable()->references('id')->on($tableName)->nullOnDelete();
         });
       }
-      if (! Schema::hasColumns($tableName, ['lft', 'rgt', 'depth'])) {
+      if (!Schema::hasColumns($tableName, ['lft', 'rgt', 'depth'])) {
         Schema::table($tableName, function (Blueprint $table) use ($tableName) {
           $table->unsignedInteger('lft')->default(0);
           $table->unsignedInteger('rgt')->default(0);
@@ -510,24 +510,28 @@ trait DataTable {
     if (static::$is_submitable ?? false) {
       Inertia::share([
         'prints' => Inertia::defer(
-          fn () => PrintTemplate::where('model', static::class)->get()),
+          fn() => PrintTemplate::where('model', static::class)->get()
+        ),
       ]);
     }
     Inertia::share([
       'translateKey' => $this->translateKey ?? null,
       'connections'  => Inertia::defer(
         function () {
-          $data = \collect(ModelConnection::search(static::class, $this->getKey())
-            ->get()
-            ->toArray())
+          $data = \collect(
+            ModelConnection::search(static::class, $this->getKey())
+              ->get()
+              ->toArray(),
+          )
             ->groupBy('reference_type')
-            ->mapWithKeys(function ($connections) {
+            ->map(function ($connections) {
               $reference_type = $connections[0]["reference_type"];
               $model          = new $reference_type;
+              $nameModel      = Str::title(Str::replace("_", " ", Str::snake(value: $model->getNameClass())));
               $connections    = $connections->unique('reference_id');
-              return [[
+              return [
                 'reference_type' => $reference_type,
-                'model'          => Str::title(Str::replace("_", " ", Str::snake($model->getNameClass()))),
+                'model'          => $nameModel,
                 'count'          => count($connections),
                 'route'          => Str::plural($model->getNameClass()) . ".index",
                 'query'          => [],
@@ -538,8 +542,9 @@ trait DataTable {
                     'route' => Str::plural($model->getNameClass()) . ".show",
                   ];
                 }),
-              ]];
-            });
+              ];
+            })
+            ->values();
           return $data;
           // return \array_map
           // ->map(function ($connection) {
@@ -551,20 +556,24 @@ trait DataTable {
           //     'query' => []
           //   ];
           //   });
-        }),
+        }
+      ),
       'logs'         => Inertia::defer(
-        fn () => Log::with('user')
+        fn() => Log::with('user')
           ->where('loggable_type', static::class)
           ->where('loggable_id', operator: $this->id)
           ->orderByDesc('created_at')
           ->get(),
-        'logs'),
+        'logs',
+      ),
       'tags'         => Inertia::defer(
-        fn () => $this->tags()->get(['id', 'name']),
-        'tags'),
+        fn() => $this->tags()->get(['id', 'name']),
+        'tags',
+      ),
       'attachments'  => Inertia::defer(
-        fn () => $this->files()->get(['id', 'name']),
-        'attachments'),
+        fn() => $this->files()->get(['id', 'name']),
+        'attachments',
+      ),
     ]);
   }
 
