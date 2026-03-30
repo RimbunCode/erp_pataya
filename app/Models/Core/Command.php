1ae3bce3e5e848a6dedc227ee6e9ab20daf1bf2b
rahmadmrhd <82705093+rahmadmrhd@users.noreply.github.com>
2026-03-30 09:54:07 +0000

    fix: Fix code style by Laravel Pint

--- app/Models/Core/Command.php
+++ app/Models/Core/Command.php
@@ -6,12 +6,10 @@ use App\Models\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Command extends Model
-{
+class Command extends Model {
     use HasUlids, SoftDeletes;
 
     //
 
     protected $guarded = ['id'];
-
 }
