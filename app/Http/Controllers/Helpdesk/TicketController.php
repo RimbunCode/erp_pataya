c74537146b80ab6004fba79ccc283cb3573f1a9a
rahmadmrhd <82705093+rahmadmrhd@users.noreply.github.com>
2026-06-28 11:12:43 +0000

    fix: apply lint fixer (eslint + pint)

--- app/Http/Controllers/Helpdesk/TicketController.php
+++ app/Http/Controllers/Helpdesk/TicketController.php
@@ -22,7 +22,7 @@ class TicketController extends Controller {
     protected function enforcePermission(string $method): ?string {
         return match ($method) {
             'markDone', 'updateTicket' => 'write',
-            default => null,
+            default                    => null,
         };
     }
 
