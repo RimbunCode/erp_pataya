0877e47d30a6a76539129337d3a4360ae32729a7
rahmadmrhd <82705093+rahmadmrhd@users.noreply.github.com>
2026-07-21 05:13:01 +0000

    fix: apply lint fixer (eslint + pint)

--- config/mail.php
+++ config/mail.php
@@ -37,7 +37,7 @@ return [
 
     'mailers' => [
 
-        'smtp'       => [
+        'smtp' => [
             'transport'    => 'smtp',
             'scheme'       => env('MAIL_SCHEME'),
             'url'          => env('MAIL_URL'),
@@ -50,11 +50,11 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
-        'ses'        => [
+        'ses' => [
             'transport' => 'ses',
         ],
 
-        'postmark'   => [
+        'postmark' => [
             'transport' => 'postmark',
             // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
             // 'client' => [
@@ -62,25 +62,25 @@ return [
             // ],
         ],
 
-        'resend'     => [
+        'resend' => [
             'transport' => 'resend',
         ],
 
-        'sendmail'   => [
+        'sendmail' => [
             'transport' => 'sendmail',
             'path'      => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
         ],
 
-        'log'        => [
+        'log' => [
             'transport' => 'log',
             'channel'   => env('MAIL_LOG_CHANNEL'),
         ],
 
-        'array'      => [
+        'array' => [
             'transport' => 'array',
         ],
 
-        'failover'   => [
+        'failover' => [
             'transport' => 'failover',
             'mailers'   => [
                 'smtp',
@@ -109,7 +109,7 @@ return [
     |
     */
 
-    'from'    => [
+    'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name'    => env('MAIL_FROM_NAME', 'Example'),
     ],
