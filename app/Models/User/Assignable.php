ec8704ccb9389fbc9073f84141c0534e9ba174c3
rahmadmrhd <82705093+rahmadmrhd@users.noreply.github.com>
2026-07-29 08:03:14 +0000

    fix: apply lint fixer (eslint + pint)

--- app/Models/User/Assignable.php
+++ app/Models/User/Assignable.php
@@ -5,10 +5,10 @@ namespace App\Models\User;
 use App\Models\Model;
 
 class Assignable extends Model {
-    protected $table        = 'assignables';
-    public    $timestamps   = false;
-    public    $incrementing = false;
-    protected $keyType      = 'string';
+    protected $table     = 'assignables';
+    public $timestamps   = false;
+    public $incrementing = false;
+    protected $keyType   = 'string';
 
     public static function templateLink() {
         return '<title>:name (:type)</title><b>:name</b><br/><span>:type</span>';
