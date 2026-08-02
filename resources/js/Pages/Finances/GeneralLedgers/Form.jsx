645fd0cc48010f5d21a19a2f64c58d8bf0263bf9
Jonathan Imanuel <jonathanimanuel023@gmail.com>
2026-08-02 18:30:42 +0700

    fix(finances): cleanup General Ledger form component
    - Remove unused BranchLinkModel import
    - Remove unused Input component import
    - Remove code field from form display
    - Comment out branch field section temporarily
    - Reformat FormInput components for better readability
    - Simplify AccountLinkModel and against_account field formatting

--- resources/js/Pages/Finances/GeneralLedgers/Form.jsx
+++ resources/js/Pages/Finances/GeneralLedgers/Form.jsx
@@ -1,10 +1,10 @@
 import { FormPageContent, useFormPage } from "@/Pages/Core/FormPage";
 
 import AccountLinkModel from "../Accounts/AccountLinkModel";
-import BranchLinkModel from "@/Pages/Settings/Branches/BranchLinkModel";
+
 import DatetimePicker from "@/Components/DatetimePicker";
 import FormInput from "@/Components/FormInput";
-import { Input } from "@/Components/ui/input";
+
 import NumberInput from "@/Components/NumberInput";
 import { useLaravelReactI18n } from "laravel-react-i18n";
 
@@ -15,18 +15,11 @@ export default function Form() {
   return (
     <FormPageContent value="detail" title={t("finances.generalLedger.detail")}>
       <div className="grid gap-x-4 gap-y-4 md:grid-cols-2">
-        <FormInput name="code" label={t("finances.generalLedger.columns.code")}>
-          <Input value={data?.code} readOnly />
-        </FormInput>
         <FormInput
           name="account"
           label={t("finances.generalLedger.columns.account")}
         >
-          <AccountLinkModel
-            value={data?.account}
-            readOnly
-            disabledAddButton
-          />
+          <AccountLinkModel value={data?.account} readOnly disabledAddButton />
         </FormInput>
         <FormInput
           name="against_account"
@@ -38,16 +31,22 @@ export default function Form() {
             disabledAddButton
           />
         </FormInput>
-        <FormInput
+        {/* <FormInput
           name="branch"
           label={t("finances.generalLedger.columns.branch")}
         >
           <BranchLinkModel value={data?.branch} readOnly disabledAddButton />
-        </FormInput>
-        <FormInput name="debit" label={t("finances.generalLedger.columns.debit")}>
+        </FormInput> */}
+        <FormInput
+          name="debit"
+          label={t("finances.generalLedger.columns.debit")}
+        >
           <NumberInput value={data?.debit} readOnly decimalScale={2} />
         </FormInput>
-        <FormInput name="credit" label={t("finances.generalLedger.columns.credit")}>
+        <FormInput
+          name="credit"
+          label={t("finances.generalLedger.columns.credit")}
+        >
           <NumberInput value={data?.credit} readOnly decimalScale={2} />
         </FormInput>
         <FormInput
